cdff817388bf817eac82aa8d63c760b69ef8043e
DAVI HUGO DOMINICKI <davi.dominicki@cs.up.edu.br>
2026-04-24 21:42:15 -0300

    Parte POO

---
+++ model/Usuario.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Model;
+
+class Usuario{
+    private int $id;
+    private string $email, $senha;
+
+    public function __construct($id=0, $email="", $senha=""){
+        $this->id = $id;
+        $this->email = $email;
+        $this->$senha = $senha;
+  
+    }
+
+    public function __get($atributo){
+        return $this->$atributo;
+    }
+    public function __set($atributo, $valor){
+        $this->$atributo = $valor;
+    }
+
+
+}
